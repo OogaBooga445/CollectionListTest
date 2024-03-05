505097a313fb3b0e1c93c7e69bdf2f1da1b7d248
OogaBooga445 <117642804+OogaBooga445@users.noreply.github.com>
2024-03-05 12:20:04 +0000

    init

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World and RVT!");
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
